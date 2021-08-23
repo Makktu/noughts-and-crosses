d868f89a36968182a4405b7d9fc9f56d9def2efa
Makktu <johnmcna@gmail.com>
2021-08-23 19:18:22 +0100

    more messing about

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function newUser(name, level) {
+function New_user(name, level) {
     this.name = name;
 
     level === 0 ? (this.isAdmin = false) : (this.isAdmin = true);
 }
 
-let user1 = new newUser("John", 1);
+let user1 = new New_user("Dee", 0);
 
-console.log(user1);
+console.log(`${user1.name} ${user1.isAdmin ? "is " : "is not "}Admin`);
