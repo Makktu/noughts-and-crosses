065fc63572e6eb57406a9b7b7e4d4b1b677fa214
Makktu <johnmcna@gmail.com>
2021-08-23 19:14:19 +0100

    baby steps...

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function newUser(name, level) {
+    this.name = name;
+
+    level === 0 ? (this.isAdmin = false) : (this.isAdmin = true);
+}
+
+let user1 = new newUser("John", 1);
+
+console.log(user1);
