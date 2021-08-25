70ff569cf72183611fa910fe72012aa76918722b
Makktu <johnmcna@gmail.com>
2021-08-25 16:47:12 +0100

    Vague progress

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 "use strict";
 
-function New_user(name, level) {
-    this.name = name;
+// function New_user(name, level) {
+//     this.name = name;
 
-    level === 0 ? (this.isAdmin = false) : (this.isAdmin = true);
-}
+//     level === 0 ? (this.isAdmin = false) : (this.isAdmin = true);
+// }
 
-let user1 = new New_user("Dee", 0);
+// let user1 = new New_user("Dee", 0);
 
-console.log(`${user1.name} ${user1.isAdmin ? "is " : "is not "}Admin`);
+// console.log(`${user1.name} ${user1.isAdmin ? "is " : "is not "}Admin`);
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    id: 5566,
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    },
+};
+
+console.log(Object.values(person));
+
+person.fullName = person.fullName.toString();
+
+let myJsonString = JSON.stringify(person);
+
+console.log(myJsonString);
